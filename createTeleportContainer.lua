6e8b47852697c5ade613b51036cac75b115a8047
Ameera0786 <122122378+Ameera0786@users.noreply.github.com>
2025-11-12 09:52:34 -0600

    Update createTeleportContainer.lua

--- createTeleportContainer.lua
+++ createTeleportContainer.lua
@@ -4,6 +4,7 @@ local font = Enum.Font.FredokaOne
 
 -- Create teleportccontainer
 local function createContainer() 
+	-- Create container
 	local container = Instance.new("Frame")
 	container.AnchorPoint = Vector2.new(0.5, 0.5)
 	container.BackgroundColor3 = Color3.fromRGB(50, 50, 50)
@@ -12,6 +13,7 @@ local function createContainer()
 	container.Size = UDim2.new(0.55, 0, 0.35, 0)
 	container.Visible = false
 
+	-- Create corner and padding
 	local corner = Instance.new("UICorner")
 	
 	local padding = Instance.new("UIPadding")
@@ -19,47 +21,57 @@ local function createContainer()
 	padding.PaddingLeft = UDim.new(0, 10)
 	padding.PaddingRight = UDim.new(0, 10)
 	padding.PaddingBottom = UDim.new(0, 10)
+	
+	-- Set parents
+	corner.Parent = container
+	padding.Parent = container
+	container.Parent = GUI
+	return container
+end
 
-	local text = Instance.new("TextLabel")
-	text.AnchorPoint = Vector2.new(0.5, 0.5)
-	text.Position = UDim2.new(0.5, 0, -0.1, 0)
-	text.Size = UDim2.new(0.5, 0, 0.15, 0)
-	text.Font = font
-	text.TextColor3 = Color3.fromRGB(255, 255, 255)
-	text.Text = "Teleportation Hub"
-	text.TextScaled = true
-	text.TextSize = 10
-	
+-- Create title label
+local function createTitle(container)
+	-- Create label
+	local title = Instance.new("TextLabel")
+	title.AnchorPoint = Vector2.new(0.5, 0.5)
+	title.Position = UDim2.new(0.5, 0, -0.1, 0)
+	title.Size = UDim2.new(0.5, 0, 0.15, 0)
+	title.Font = font
+	title.TextColor3 = Color3.fromRGB(255, 255, 255)
+	title.Text = "Teleportation Hub"
+	title.TextScaled = true
+	title.TextSize = 10
+
+	-- Create stroke and corner
 	local stroke = Instance.new("UIStroke")
+
+	local corner = Instance.new("UICorner")
 	
-	local corner2 = Instance.new("UICorner")
-	
+	-- Set parents
+	title.Parent = container
+	stroke.Parent = title
+	corner.Parent = title
+end
+
+-- Create scroll
+local function createScroll(container)
+	-- Create scroll and layout
 	local scroll = Instance.new("ScrollingFrame")
 	scroll.BackgroundTransparency = 1
 	scroll.Name = "ScrollingFrame"
 	scroll.ScrollBarThickness = 8
 	scroll.Size = UDim2.new(1, 0, 1, 0)
-	
+
 	local listLayout = Instance.new("UIListLayout")
 	listLayout.Padding = UDim.new(0, 20)
-	
-	corner.Parent = container
-	padding.Parent = container
-	text.Parent = container
-	stroke.Parent = text
-	corner2.Parent = text
+
+	-- Set parents
 	scroll.Parent = container
 	listLayout.Parent = scroll
-	container.Parent = GUI
-	return container
 end
 
-local function createMapTeleportor(containerScrollFrame, mapName, index) 
-	-- Container frame
-	local worldFrame = Instance.new("Frame")
-	worldFrame.Size = UDim2.new(0.9, 0, 0.17, 0)
-	worldFrame.BackgroundTransparency = 1
-	
+-- Create teleport button
+local function createTeleportButton(frame)
 	-- Teleport button
 	local btn = Instance.new("TextButton")
 	btn.BackgroundColor3 = Color3.fromRGB(100, 100, 100)
@@ -72,22 +84,31 @@ local function createMapTeleportor(containerScrollFrame, mapName, index)
 	btn.TextColor3 = Color3.fromRGB(255, 255, 255)
 	btn.Font = font
 	btn.TextScaled = true
-	
-	-- Padding, scale, stroke, and corner
+
+	-- Corner, padding, scale, and stroke
+	local corner = Instance.new("UICorner")
+
 	local padding = Instance.new("UIPadding")
 	padding.PaddingTop = UDim.new(0.02, 0)
 	padding.PaddingLeft = UDim.new(0.02, 0)
 	padding.PaddingRight = UDim.new(0.02, 0)
 	padding.PaddingBottom = UDim.new(0.02, 0)
 
-	
 	local scale = Instance.new("UIScale")
 	scale.Scale = 0.7
-	
+
 	local stroke = Instance.new("UIStroke")
 
-	local corner = Instance.new("UICorner")
-	
+	-- Set parents
+	corner.Parent = btn
+	padding.Parent = btn
+	scale.Parent = btn
+	stroke.Parent = btn
+	btn.Parent = frame
+end
+
+-- Create world icon
+local function createIcon(frame, index)
 	-- World icon and world number
 	local imgLabel = Instance.new("ImageLabel")
 	imgLabel.Position = UDim2.new(0, 0, 0, 0)
@@ -95,7 +116,7 @@ local function createMapTeleportor(containerScrollFrame, mapName, index)
 	imgLabel.Size = UDim2.new(1, 0, 1, 0)
 	imgLabel.Image = "rbxassetid://138929576830797"
 	imgLabel.ScaleType = Enum.ScaleType.Fit
-	
+
 	-- Set number for each world icon
 	local worldLabel = Instance.new("TextLabel")
 	worldLabel.AnchorPoint = Vector2.new(0.5, 0.5)
@@ -107,11 +128,19 @@ local function createMapTeleportor(containerScrollFrame, mapName, index)
 	worldLabel.Font = font
 	worldLabel.Text = index
 	worldLabel.TextScaled = true
-	
+
 	-- Image ratio
 	local ratio = Instance.new("UIAspectRatioConstraint")
 	ratio.AspectRatio = 0.7
 	
+	-- Set parents
+	imgLabel.Parent = frame
+	worldLabel.Parent = imgLabel
+	ratio.Parent = imgLabel
+end
+
+-- Create label for current world name
+local function createWorldLabel(frame, mapName)
 	-- Text label indicating world name
 	local textLabel = Instance.new("TextLabel")
 	textLabel.AnchorPoint = Vector2.new(0.5, 0.5)
@@ -124,33 +153,42 @@ local function createMapTeleportor(containerScrollFrame, mapName, index)
 	textLabel.Text = mapName
 	textLabel.Name = mapName.."Label"
 	textLabel.TextScaled = true
-	
-	-- Stroke, corner, and scale
-	local stroke2 = Instance.new("UIStroke")
-	
-	local corner2 = Instance.new("UICorner")
 
-	local scale2 = Instance.new("UIScale")
-	scale2.Scale = 0.6
-	
+	-- Stroke, corner, and scale
+	local stroke = Instance.new("UIStroke")
+
+	local corner = Instance.new("UICorner")
+
+	local scale = Instance.new("UIScale")
+	scale.Scale = 0.6
+
 	-- Set parents
-	btn.Parent = worldFrame
-	corner.Parent = btn
-	padding.Parent = btn
-	scale.Parent = btn
-	stroke.Parent = btn
-	imgLabel.Parent = worldFrame
-	worldLabel.Parent = imgLabel
-	ratio.Parent = imgLabel
-	textLabel.Parent = worldFrame
-	stroke2.Parent = textLabel
-	corner2.Parent = textLabel
-	scale2.Parent = textLabel
+	textLabel.Parent = frame
+	stroke.Parent = textLabel
+	corner.Parent = textLabel
+	scale.Parent = textLabel
+end
+
+-- Create world rows
+local function createMapTeleportor(containerScrollFrame, mapName, index) 
+	-- Container frame
+	local worldFrame = Instance.new("Frame")
+	worldFrame.Size = UDim2.new(0.9, 0, 0.17, 0)
+	worldFrame.BackgroundTransparency = 1
+	
+	-- Set parent
 	worldFrame.Parent = containerScrollFrame
+	
+	-- Helper functions to design frame
+	createIcon(worldFrame, index)
+	createTeleportButton(worldFrame)
+	createWorldLabel(worldFrame, mapName)
 end
 
 -- Create and populate teleport container
 teleportContainer = createContainer()
+createTitle(teleportContainer)
+createScroll(teleportContainer)
 maps = {"World 1", "World 2", "World 3"}
 scrollingFrame = teleportContainer:WaitForChild("ScrollingFrame")
 for i = 1, #maps do
